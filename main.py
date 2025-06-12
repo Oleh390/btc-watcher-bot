4765755dc131be7ef7f531beafaf60c166014b5a
Oleh390 <originalplus01@gmail.com>
2025-06-12 19:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import asyncio
 from telegram import Bot, Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from news_handler import fetch_all_news, format_news_message
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = int(os.getenv("TELEGRAM_USER_ID"))
 
@@ -17,20 +18,25 @@ async def send_news():
         msg = format_news_message(item)
         await bot.send_message(chat_id=CHAT_ID, text=msg, parse_mode="HTML")
 
-async def news_command(update: Update, context):
+# Команда вручную
+async def news_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await send_news()
 
-async def run_scheduler():
+# Фоновая задача каждые 10 минут
+async def news_scheduler(app):
     while True:
         await send_news()
-        await asyncio.sleep(600)  # 10 минут
+        await asyncio.sleep(600)
 
-if __name__ == '__main__':
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("news", news_command))
 
-    # Запускаем планировщик в фоне
-    asyncio.create_task(run_scheduler())
+    # запускаем фоновую задачу
+    app.create_task(news_scheduler(app))
 
-    # Запускаем самого бота (главный цикл)
-    app.run_polling()
+    # запускаем самого бота
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
